88ce8a0d708d9de2d948a112bc0f047dbb8100ec
Mat-Araujo <matheus.oliveiraraujo@hotmail.com>
2024-05-01 18:59:09 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-let nome = "MOA";
+let nome = "Raze"
+let nivel = 2500
 
-console.log(nome);
+if(nivel <= 1000){
+        nivel = "Ferro"
+    }else if(nivel <= 2000){
+        nivel = "Bronze"
+    }else if(nivel <= 5000){
+        nivel = "Prata"
+    }else if(nivel <= 7000){
+        nivel = "Ouro"
+    }else if(nivel <= 8000){
+        nivel = "Platina"
+    }else if(nivel <= 9000){
+        nivel = "Ascendente"
+    }else if(nivel <= 10000){
+        nivel = "Imortal"
+    }else if(nivel > 10001){
+        nivel = "Radiante"
+    }
+
+console.log("A agente " + nome + " está no elo " + nivel)
